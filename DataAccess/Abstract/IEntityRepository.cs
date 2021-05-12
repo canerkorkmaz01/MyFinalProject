68864e25e52f50e42d7e326c1646d6c3a84a9069
Caner KORKMAZ <caner.korkmaz@hotmail.com>
2021-05-12 16:41:43 +0300

    1

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where  T:class,IEntity,new ()
+    {
+        //generic constraint
+        //class : referans tip
+        //IEntity : IEntity Olabilir veya IEntity  implemente eden bir nesne olaiblir 
+        //new () : new'lenebilir olmalı
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        //List<T> GetAllByCategory(int categoryId);
+    }
+}
---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public int MyProperty { get; set; }
+        public int City { get; set; }
+
+    }
+}
