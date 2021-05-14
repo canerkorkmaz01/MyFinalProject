22c1f6922143431b198122eef941a768470f8a8c
Caner KORKMAZ <caner.korkmaz@hotmail.com>
2021-05-14 12:39:33 +0300

    2

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.DataAccess.EntityFramework
 {
-    public interface IEntity
+    class EfEntityRepositoryBase
     {
     }
 }
