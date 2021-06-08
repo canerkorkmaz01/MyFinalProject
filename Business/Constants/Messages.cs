d3ee7fcf3f86aa5e0ab223e6747a8639907820fd
Caner KORKMAZ <caner.korkmaz@hotmail.com>
2021-06-09 02:42:20 +0300

    09/06/2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +15,13 @@ namespace Business.Constants
         public static string ProductCountCategoryError = "Bir Kategoride En Fazla 10 Ürün Olabilir";
         public static string ProductNameAlreadyExists = "Bu İsimde Zaten Başka Bir Ürün Var.";
         public static string CategoryLimitExceded = "Kategori Limiti Aşıldıgı İçin Yeni Ürün Eklenmiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+
     }
 }
---
+++ Core/Utilities/IoC/ServiceTool.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.IoC
+{/// <summary>
+///Masaüstü ve Diger Uygulamlarda Çagırmak İçin Kullanılan Yöntem 
+/// </summary>
+    public static class ServiceTool
+    {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        public static IServiceCollection Create(IServiceCollection services)
+        {
+            ServiceProvider = services.BuildServiceProvider();
+            return services;
+        }
+    }
+}
